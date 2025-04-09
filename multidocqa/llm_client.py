b21a7d6996d6923ae1a6ebbb5664f17a4fa8ada1
nilesh-c <nchakn@gmail.com>
2025-04-09 10:31:55 +0000

    JSON output; VllmEndpointGenerator returns generator/iterator

--- multidocqa/llm_client.py
+++ multidocqa/llm_client.py
@@ -9,28 +9,32 @@ class VllmEndpointGenerator:
         self,
         model: str,
         openai_endpoint_url: str = "http://localhost:8000/v1",
-        max_concurrent_requests: int = 10,
+        max_concurrent_requests: int = 5,
     ) -> None:
         self._model_name = model
         self._client = self._get_client(openai_endpoint_url)
         self._semaphore = asyncio.Semaphore(max_concurrent_requests)
 
-    async def generate(self, texts: List[str]) -> List[str]:
+    async def generate(self, texts: List[str], guided_json=None) -> List[str]:
         response_coroutines = []
         for text in texts:
-            response = self._get_response(text)
+            response = self._get_response(text, guided_json)
             response_coroutines.append(response)
 
-        responses = await asyncio.gather(*response_coroutines)
-        return responses
+        for future in asyncio.as_completed(response_coroutines):
+            result = await future
+            yield result
 
     def _get_client(self, endpoint_url: str):
         return AsyncOpenAI(base_url=endpoint_url, api_key="empty")
 
-    async def _get_response(self, text: str):
+    async def _get_response(self, text: str, guided_json=None):
+        extra_body = {"guided_json": guided_json} if guided_json else None
         async with self._semaphore:
             response = await self._client.chat.completions.create(
-                model=self._model_name, messages=[{"role": "user", "content": text}]
+                model=self._model_name,
+                messages=[{"role": "user", "content": text}],
+                extra_body=extra_body,
             )
 
             return (
