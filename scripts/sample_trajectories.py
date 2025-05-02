66402b231767cdc67c57c387f5b66e293fb691c3
nilesh-c <nchakn@gmail.com>
2025-05-02 15:48:24 +0000

    More efficient processing and better error handling

--- scripts/sample_trajectories.py
+++ scripts/sample_trajectories.py
@@ -1,12 +1,13 @@
 import asyncio
-import os
 import json
-import random
+import numpy as np
 from tqdm import tqdm
 from tqdm.asyncio import tqdm_asyncio
 
 from multidocqa.llm_client import VllmEndpointGenerator
 from multidocqa.utils import compute_reward, load_data, create_simple_prompt
+from pydantic import BaseModel
+from typing import Any
 
 # Paths
 CIVIL_CODE_FILE = (
@@ -18,44 +19,109 @@ TRAJECTORY_SAVE_PATH = "data/trajectories.jsonl"
 MODEL_NAME = "deepseek-ai/DeepSeek-R1-Distill-Qwen-14B"
 
 # Hyperparameters
-N_SAMPLES_PER_INPUT = 100
+N_SAMPLES_PER_INPUT = 500
+
+
+class DataPoint(BaseModel):
+    idx: int
+    prompt: str
+    label: str
+
+
+async def process_outputs(outputs, data_point: DataPoint):
+    async for output in outputs:
+        samples = []
+        for prediction, reasoning in output:
+            reward = compute_reward(prediction, data_point.label)
+            samples.append(
+                {"reasoning": reasoning, "prediction": prediction, "reward": reward}
+            )
+
+    return data_point, samples
+
+
+async def process_chunk(generator: VllmEndpointGenerator, chunk: list[DataPoint]):
+    """Process a single chunk of prompts."""
+    outputs = []
+    for seed in range(0, int(N_SAMPLES_PER_INPUT / 10)):
+        for prompt in chunk:
+            output = generator.generate(
+                [prompt.prompt],
+                n=int(N_SAMPLES_PER_INPUT / 10),
+                temperature=0.3,
+                top_p=1.0,
+                seed=seed,
+                timeout=1000,
+            )
+            outputs.append((prompt, output))
+
+    tasks = [process_outputs(output, prompt) for prompt, output in outputs]
+
+    # Wait for all outputs to complete
+    results = []
+    for coroutine in tqdm_asyncio.as_completed(tasks):
+        try:
+            result = await coroutine
+            results.append(result)
+        except Exception as e:
+            print("Got an exception:", e)
+
+    grouped_results = dict()
+    for data_point, output_samples in results:
+        if data_point.idx not in grouped_results:
+            grouped_results[data_point.idx] = {
+                "idx": data_point.idx,
+                "prompt": data_point.prompt,
+                "label": data_point.label,
+                "samples": [],
+            }
+        grouped_results[data_point.idx]["samples"].extend(output_samples)
+
+    return list(grouped_results.values())
+
+
+async def save_trajectories(prompts: list[DataPoint]):
+    # Open output file
+    with open(TRAJECTORY_SAVE_PATH, "w", encoding="utf-8") as fout:
+        generators = [
+            VllmEndpointGenerator(
+                model=MODEL_NAME,
+                openai_endpoint_url=f"http://{host}:8000/v1",
+                max_concurrent_requests=30,
+            )
+            for host in ["ecdpl01", "ecdpl02", "ecdpl03"]
+        ]
+
+        print("Sending prompts to model...")
+        # Create tasks for processing chunks concurrently
+        tasks = []
+        for idx, chunk in enumerate(np.array_split(prompts, 3)):
+            tasks.append(process_chunk(generators[idx % 3], chunk.tolist()))
+
+        # Wait for all tasks to complete
+        results_list = await asyncio.gather(*tasks)
+
+        # Write results to file
+        for results in results_list:
+            for result in results:
+                fout.write(json.dumps(result) + "\n")
+
+    print(f"Saved all trajectories to {TRAJECTORY_SAVE_PATH}")
 
 
 def main():
     dataset = load_data(DATASET_FILE)
 
     create_prompt = create_simple_prompt
-    prompts = [create_prompt(item["articles"], item["question"]) for item in dataset][
-        :10
-    ]
-    asyncio.run(save_trajectories(prompts, dataset))
-
-
-async def save_trajectories(prompts, dataset):
-    # Open output file
-    with open(TRAJECTORY_SAVE_PATH, "w", encoding="utf-8") as fout:
-        generator = VllmEndpointGenerator(model=MODEL_NAME)
-        print("Sending prompts to model...")
-        outputs = generator.generate(
-            prompts, n=N_SAMPLES_PER_INPUT, temperature=0.3, top_p=1.0
+    prompts = [
+        DataPoint(
+            idx=idx,
+            prompt=create_prompt(item["articles"], item["question"]),
+            label=item["label"],
         )
-
-        idx = 0
-        async for outputs in tqdm_asyncio(outputs, total=len(prompts)):
-            dataset_item = dataset[idx]
-            prompt = prompts[idx]
-            idx += 1
-
-            samples = []
-            for prediction, reasoning in outputs:
-                reward = compute_reward(prediction, dataset_item["label"])
-                samples.append(
-                    {"reasoning": reasoning, "prediction": prediction, "reward": reward}
-                )
-
-            fout.write(json.dumps({"prompt": prompt, "samples": samples}) + "\n")
-
-    print(f"Saved all trajectories to {TRAJECTORY_SAVE_PATH}")
+        for idx, item in enumerate(dataset)
+    ]
+    asyncio.run(save_trajectories(prompts))
 
 
 if __name__ == "__main__":
